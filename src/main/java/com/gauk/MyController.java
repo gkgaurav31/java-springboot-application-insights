c2c3ae3d0b6ac7f447c4d4b48175774f63cf8b85
gkgaurav31 <gkgaurav31@gmail.com>
2020-05-03 02:56:18 +0530

    added mysql snat test

--- src/main/java/com/gauk/MyController.java
+++ src/main/java/com/gauk/MyController.java
@@ -1,8 +1,11 @@
 package com.gauk;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microsoft.applicationinsights.TelemetryClient;
@@ -16,10 +19,44 @@ public class MyController {
 	@Autowired
 	TelemetryClient telemetryClient;
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private MySQLThread mySQLThread;
+	
 	@GetMapping(value = "/hello")
 	public String hello() {
 		return "Hello";
 	}
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+
+	@PostMapping(path="/add") // Map ONLY POST Requests
+	public @ResponseBody String addNewUser (@RequestParam String name
+			, @RequestParam String email) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		User n = new User();
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Saved";
+	}
+
+	@GetMapping(value = "/mysql")
+	public String StringSnatMySQLTest(@RequestParam(name = "count") int count){
+		for(int i=0; i<count; i++) {
+			new Thread(mySQLThread).start();
+		}
+		return "Done";
+	}
+	
 
 	@GetMapping(value = "/snat")
 	public String snatTest(@RequestParam(name = "uri") String uri, @RequestParam(name="count") int count) {
@@ -75,4 +112,22 @@ class MyThread implements Runnable{
 		System.out.println(Thread.currentThread().getName() + " running...");
 	}
 
+}
+
+@Component
+class MySQLThread implements Runnable{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run() {
+		User n = new User();
+		n.setName("test");
+		n.setEmail("test@gmail.com");
+		System.out.println("userRepository: " + userRepository);
+		System.out.println("user::" + n);
+		userRepository.save(n);
+	}
+	
 }
